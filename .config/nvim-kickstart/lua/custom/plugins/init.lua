d263096fbcf46a4ffe102cb4c578c72e5e9f3bd3
brittmouse <bstancoff@proton.me>
2024-03-08 18:21:55 -0500

    lazy (update), kickstart

---
+++ .config/nvim-kickstart/lua/custom/plugins/init.lua
@@ -0,0 +1,56 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'folke/twilight.nvim' },
+  {
+    'folke/zen-mode.nvim',
+    opts = {},
+  },
+  {
+    -- MarkdownPreview
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+      {
+        '<leader>dp',
+        ft = 'markdown',
+        '<cmd>MarkdownPreviewToggle<CR>',
+        desc = '[D]ocument [P]review (Markdown)',
+      },
+    },
+    config = function()
+      vim.cmd [[do FileType]]
+    end,
+  },
+
+  {
+    -- Headlines for in-editor rendering
+    'lukas-reineke/headlines.nvim',
+    opts = function()
+      local opts = {}
+      for _, ft in ipairs { 'markdown', 'norg', 'org', 'rmd' } do
+        opts[ft] = {
+          headline_highlights = {},
+        }
+        for i = 1, 6 do
+          local hl = 'Headline' .. i
+          vim.api.nvim_set_hl(0, hl, { link = 'Headline', default = true })
+          table.insert(opts[ft].headline_highlights, hl)
+        end
+      end
+      return opts
+    end,
+    ft = { 'markdown', 'norg', 'org', 'rmd' },
+    config = function(_, opts)
+      vim.schedule(function()
+        require('headlines').setup(opts)
+        require('headlines').refresh()
+      end)
+    end,
+  },
+}
