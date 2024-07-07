b8380c16a0a5683ffa9c747e59d5c0a5efdef318
brittmouse <bstancoff@proton.me>
2024-07-07 11:12:26 -0400

    nvim: move kickstart to 'nvim-kickstart', fresh install of NvChad in nvim

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,12 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("n", "[b", "<CMD>bprevious<CR>", { desc = "Prev buffer"} )
+map("n", "]b", "<CMD>bnext<CR>", { desc = "Next buffer"} )
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    -- MarkdownPreview
+    'iamcco/markdown-preview.nvim',
+    lazy = true,
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+      {
+        '<leader>dp',
+        ft = 'markdown',
+        '<cmd>MarkdownPreviewToggle<CR>',
+        desc = '[D]ocument [P]review (Markdown)',
+      },
+    },
+    config = function()
+      vim.cmd [[do FileType]]
+    end,
+  },
+  {
+    -- Headlines for in-editor rendering
+    'lukas-reineke/headlines.nvim',
+    lazy = true,
+    opts = function()
+      local opts = {}
+      for _, ft in ipairs { 'markdown', 'norg', 'org', 'rmd' } do
+        opts[ft] = {
+          headline_highlights = {},
+        }
+        for i = 1, 6 do
+          local hl = 'Headline' .. i
+          vim.api.nvim_set_hl(0, hl, { link = 'Headline', default = true })
+          table.insert(opts[ft].headline_highlights, hl)
+        end
+      end
+      return opts
+    end,
+    ft = { 'markdown', 'norg', 'org', 'rmd' },
+    config = function(_, opts)
+      vim.schedule(function()
+        require('headlines').setup(opts)
+        require('headlines').refresh()
+      end)
+    end,
+  },
+  {
+    -- Vim Markdown
+    'preservim/vim-markdown',
+    lazy = true,
+  },
+  {
+    -- Glow
+    'ellisonleao/glow.nvim',
+    lazy = true,
+    config = true,
+    cmd = 'Glow',
+  },
+}
