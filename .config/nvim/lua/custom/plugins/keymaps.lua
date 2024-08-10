7dfd028735acc6e632fe45f366e24c498eba6bc7
brittmouse <bstancoff@proton.me>
2024-08-10 10:40:32 -0400

    nvim: add two useful keyboard shortcuts for saving

--- .config/nvim/lua/custom/plugins/keymaps.lua
+++ .config/nvim/lua/custom/plugins/keymaps.lua
@@ -2,4 +2,6 @@ return {
   vim.keymap.set('n', '<C-u>', '<C-u>zz'),
   vim.keymap.set('n', '<C-d>', '<C-d>zz'),
   vim.keymap.set('i', '<esc>', '<esc><cmd>update<CR>'),
+  vim.keymap.set('i', '<C-c>', '<C-c><cmd>update<CR>'),
+  vim.keymap.set('n', '<C-s>', '<cmd>update<CR>'),
 }
