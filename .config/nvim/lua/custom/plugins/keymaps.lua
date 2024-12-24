c0bee7ece99b969fce0fd7ba7ef3f81e2fbd7607
brittmouse <bstancoff@proton.me>
2024-12-24 09:48:59 -0500

    edit(nvim): remove notify and mini.starter, add oil and snacks

--- .config/nvim/lua/custom/plugins/keymaps.lua
+++ .config/nvim/lua/custom/plugins/keymaps.lua
@@ -2,6 +2,7 @@ return {
   vim.keymap.set('n', '<C-u>', '<C-u>zz'),
   vim.keymap.set('n', '<C-d>', '<C-d>zz'),
   vim.keymap.set('n', '<leader>z', 'vipzf<cmd>mkview<cr>', { desc = 'Fold Paragraph' }),
-  vim.keymap.set('i', '<esc>', '<esc><cmd>update<CR>'),
-  vim.keymap.set('i', '<C-c>', '<C-c><cmd>update<CR>'),
+  vim.keymap.set({ 'n', 'i' }, '<C-s>', '<esc><cmd>update<cr>'),
+  -- vim.keymap.set('i', '<esc>', '<esc><cmd>update<CR>'),
+  -- vim.keymap.set('i', '<C-c>', '<C-c><cmd>update<CR>'),
 }
